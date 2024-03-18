6b1942322a2735dc159ecd7edd76f98ce1e38d4a
Anais M <mtls.anais@gmail.com>
2024-03-19 00:12:22 +0100

    Main, interface code

---
+++ EMG_Explorer_App/EMGExplorer.py
@@ -0,0 +1,181 @@
+# from Explorer_package.requirement import *
+from Explorer_package import *
+import Explorer_package as exp
+from PyQt5.QtCore import Qt
+
+class Canvas(FigureCanvasQTAgg):
+    def __init__(self):
+        self.fig = Figure()
+        self.ax = self.fig.add_subplot(111)
+
+        super().__init__(self.fig)
+
+    def clear(self):
+        self.fig.clear()
+
+class EMGExplorer(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        loadUi('hdemg_viewer_exemple\Qt_creator\EMGExplorer_qt\EMGExplorer_mainwindow.ui', self)
+
+        self.actiontype_1.triggered.connect(self.oc_actiontype1)
+
+        self.show()
+
+
+    def oc_actiontype1(self):
+        self.dock = QDockWidget('dock',self)
+        self.listWidget=QListWidget()
+        self.listWidget.addItem('Item1')
+        self.listWidget.addItem('Item2')
+        self.listWidget.addItem('Item3')
+        self.listWidget.addItem('Item4')
+
+        self.dock.setWidget(self.listWidget)
+        self.dock.setFloating(False)
+        self.addDockWidget(Qt.RightDockWidgetArea,self.dock)
+
+
+def main():
+    app = QApplication(sys.argv)
+    ex = EMGExplorer()
+    ex.raise_()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+# class Example(QWidget):
+#     def __init__(self):
+#         super(Example, self).__init__()
+#         self.initUI()
+
+#     def initUI(self):
+#         hbox = QHBoxLayout(self)
+
+#         topleft = QFrame()
+#         topleft.setFrameShape(QFrame.StyledPanel)
+#         bottom = QFrame()
+#         bottom.setFrameShape(QFrame.StyledPanel)
+
+#         splitter1 = QSplitter(Qt.Horizontal)
+#         textedit = QTextEdit()
+#         splitter1.addWidget(topleft)
+#         splitter1.addWidget(textedit)
+#         splitter1.setSizes([100,200])
+
+#         splitter2 = QSplitter(Qt.Vertical)
+#         splitter2.addWidget(splitter1)
+#         splitter2.addWidget(bottom)
+
+#         hbox.addWidget(splitter2)
+
+#         self.setLayout(hbox)
+#         QApplication.setStyle(QStyleFactory.create('Cleanlooks'))
+
+#         self.setGeometry(300, 300, 300, 200)
+#         self.setWindowTitle('QSplitter demo')
+#         self.show()
+
+# def main():
+#    app = QApplication(sys.argv)
+#    ex = Example()
+#    sys.exit(app.exec_())
+	
+# if __name__ == '__main__':
+#    main()
+
+
+# class MainWindow(QMainWindow):
+#     # passer les parametres du threshold de defaut dans la creation de la fenetre
+#     #ajouter mode de traitement
+
+#     def __init__(self):
+#         super(MainWindow, self).__init__()
+
+#         # self.window = QMainWindow()
+#         loadUi('hdemg_viewer_exemple\\Qt_creator\\EMGExplorer_qt\\EMGExplorer_mainwindow.ui', self)
+#         self.show()
+
+
+# app = QApplication(sys.argv)
+# w = MainWindow()
+# app.exec_()
+
+#https://stackoverflow.com/questions/28309376/how-to-manage-qsplitter-in-qt-designer 
+
+
+
+# all in one file
+# print(dir(exp.processing_function))
+
+# import processing function
+# print(os.listdir('EMG_Explorer_App/Explorer_package/processing'))
+#['processing1.py', 'processing2.py', '__pycache__']
+
+
+# from importlib.machinery import SourceFileLoader
+ 
+# mod_list = []
+# for name in os.listdir('EMG_Explorer_App/Explorer_package/processing'):
+#     if name != '__pycache__':
+#         mod_list += [name[:-3]]
+
+# # imports the module from the given path
+# PROCESSING = {}
+# for name in mod_list:
+#     mod = SourceFileLoader(name,f"EMG_Explorer_App/Explorer_package/processing/{name}.py").load_module()
+#     PROCESSING[name] = {}
+
+#     for fc_name in dir(mod):
+#         fc = getattr(mod,fc_name)
+#         if fc_name[0] != '_':
+#             PROCESSING[name][fc_name] = fc
+
+# # print(PROCESSING)
+            
+# PROCESSING['processing1']['add']()
+
+
+
+# self.graph = pg.GraphicsLayoutWidget(border=pg.mkPen((0, 0, 0)))
+# self.timeplot = self.graph.addPlot(row=0, col=0, rowspan=1, colspan=1)
+# self.fftplot = self.graph.addPlot(row=1, col=0, rowspan=1, colspan=1)
+
+# self.timeplot.plot(x=self.time, y=self.signal, pen=pen)
+# self.timeplot.setMouseEnabled(x=True, y=False)
+# self.timeplot.setLimits(xMin=0, xMax=self.time.max())
+# self.timeplot.autoRange()
+# self.timeplot.showGrid(x=True)
+# self.timeplot.setMenuEnabled(False)
+# self.timeplot.setLabel('left', "Channel {} - EMG".format(elec_id), "")
+# self.timeplot.setLabel('bottom', "time", "s")
+# self.timeplot.sigXRangeChanged.connect(self.update_fft)
+# self.timepoint = pg.InfiniteLine(0,pen=pg.mkPen({'color': "#FF0", 'width': 4}))  # pg.CurvePoint(self.timeplot.curves[0])
+# self.timeplot.addItem(self.timepoint)
+# # arrow = pg.ArrowItem(angle=-90)
+# # arrow.setParentItem(self.timepoint)
+
+# self.fftplot.plot(self.time, self.signal, pen=pen)
+# self.fftplot.setMouseEnabled(x=True, y=False)
+# self.fftplot.showGrid(x=True)
+# self.fftplot.setMenuEnabled(False)
+# self.fftplot.setLabel('left', "Channel {} - Power".format(elec_id), "")
+# self.fftplot.setLabel('bottom', "frequency", "Hz")
+# self.fftplot.curves[0].setFftMode(True)
+# self.fftplot.enableAutoRange(True)
+
+# self.rvbox.addWidget(self.graph)
+# self.ploted = True
+
+
+# fct = 'rms'
+#         gridvar = numpy.apply_along_axis(eval("self.{}".format(fct)), 2, gridvar) #fonctionne sans le self et rms une fonction normal ?
+
+# @staticmethod
+#     def rms(x):
+#         return numpy.sqrt(x.dot(x) / x.size)
